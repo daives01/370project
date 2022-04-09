6a59f682b3555ffcf1e8f9507d4ed936d14c939f
KennethHotra <97703097+KennethHotra@users.noreply.github.com>
2022-04-09 13:43:14 -0600

    Update finger positions, add 4 positions
    Added Thumbs Up, Thumbs Down, Open Hand, and Fist Positions

--- finger_positions.py
+++ finger_positions.py
@@ -1,10 +1,63 @@
 # https://google.github.io/mediapipe/solutions/hands.html for indices
+from math import sqrt
 
-def detectFingers(hand, label): #return an array of 5 booleans, from thumb to pinky, 1 is down, 0 is up
-    isThumbClosed = hand.landmark[4].x < hand.landmark[2].x if label == "Left" else hand.landmark[4].x > hand.landmark[2].x #thumb is a little weird, because it's sideways
-    isIndexClosed = hand.landmark[8].y > hand.landmark[6].y
-    isMiddleClosed = hand.landmark[12].y > hand.landmark[10].y
-    isRingClosed = hand.landmark[16].y > hand.landmark[14].y
-    isPinkyClosed = hand.landmark[20].y > hand.landmark[18].y
-    return [isThumbClosed, isIndexClosed, isMiddleClosed, isRingClosed, isPinkyClosed]
-    
+
+def calcDistance(start, finish):
+    base = abs(start.x - finish.x)
+    height = abs(start.y - finish.y)
+    return sqrt(base ** 2 + height ** 2)
+
+
+def detectFingers(hand):  # return an array of 4 booleans, from index finger to pinky, 1 is down, 0 is up
+    # Thumb detection is too unstable in many positions to detect when it is closed
+    isIndexClosed = calcDistance(hand.landmark[8], hand.landmark[0]) < calcDistance(hand.landmark[6], hand.landmark[0])
+    isMiddleClosed = calcDistance(hand.landmark[12], hand.landmark[0]) < calcDistance(hand.landmark[10],
+                                                                                      hand.landmark[0])
+    isRingClosed = calcDistance(hand.landmark[16], hand.landmark[0]) < calcDistance(hand.landmark[14], hand.landmark[0])
+    isPinkyClosed = calcDistance(hand.landmark[20], hand.landmark[0]) < calcDistance(hand.landmark[18],
+                                                                                     hand.landmark[0])
+    return [isIndexClosed, isMiddleClosed, isRingClosed, isPinkyClosed]
+
+
+def detectOpenHand(hand):  # position exists if all fingers are open and pointed up
+    fingersClosed = detectFingers(hand)
+    fingersPointedUp = [hand.landmark[4].y < hand.landmark[3].y < hand.landmark[2].y,
+                        hand.landmark[8].y < hand.landmark[7].y < hand.landmark[6].y,
+                        hand.landmark[12].y < hand.landmark[11].y < hand.landmark[10].y,
+                        hand.landmark[16].y < hand.landmark[15].y < hand.landmark[14].y,
+                        hand.landmark[20].y < hand.landmark[19].y < hand.landmark[18].y]
+    return not any(fingersClosed) and all(fingersPointedUp)
+
+
+def detectFist(hand):  # position exists if fingers are in fist position (Thumb placement is not considered)
+    fingersClosed = detectFingers(hand)
+    fingersPointedDown = [hand.landmark[8].y > hand.landmark[7].y > hand.landmark[6].y,
+                          hand.landmark[12].y > hand.landmark[11].y > hand.landmark[10].y,
+                          hand.landmark[16].y > hand.landmark[15].y > hand.landmark[14].y,
+                          hand.landmark[20].y > hand.landmark[19].y > hand.landmark[18].y]
+    return all(fingersClosed) and all(fingersPointedDown)
+
+
+def detectThumbsUp(hand):  # position exists if thumb is pointing up and other fingers are closed
+    isThumbUp = hand.landmark[4].y < hand.landmark[3].y < hand.landmark[5].y < hand.landmark[0].y
+    fingersClosed = detectFingers(hand)
+    return isThumbUp and all(fingersClosed)
+
+
+def detectThumbsDown(hand):  # position exists if thumb is pointing down and other fingers are closed
+    isThumbDown = hand.landmark[4].y > hand.landmark[3].y > hand.landmark[5].y > hand.landmark[0].y
+    fingers = detectFingers(hand)
+    return isThumbDown and all(fingers)
+
+
+def detectPosition(hand, label):
+    if detectOpenHand(hand):
+        return "Open Hand"
+    elif detectFist(hand):
+        return "Fist"
+    elif detectThumbsUp(hand):
+        return "Thumbs Up"
+    elif detectThumbsDown(hand):
+        return "Thumbs Down"
+    else:
+        return "None Detected"
