c222722b8f3486f00d68f0e0de402e5df37218ab
Daniel Ives <danielives012@yahoo.com>
2022-04-21 18:55:40 -0600

    made code less susceptible to failure

--- pi-gesture-controller.py
+++ pi-gesture-controller.py
@@ -13,6 +13,7 @@ def main():
     try:
         HEADERSIZE = 8
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((IP, PORT))
         print("Connecting...")
         s.listen(5)
@@ -61,22 +62,39 @@ def main():
                                     hand_landmarks, results.multi_handedness[0].classification[0].label)
                                 if new_gesture == latest_gesture:
                                     if count > delay:
+                                        msg = "none"
+                                        msg = f"{len(msg):<{HEADERSIZE}}" + msg 
+                                        # SEND MESSAGE OVER SOCKET
+                                        clientsocket.send(bytes(msg, "utf-8"))
                                         continue
                                     count += 1
                                     # print(count)
                                     if count == delay:
-                                        # add length of text to start of msg
-                                        msg = f"{len(latest_gesture):<{HEADERSIZE}}" + \
-                                            latest_gesture
-                                        # SEND MESSAGE OVER SOCKET
-                                        clientsocket.send(bytes(msg, "utf-8"))
+                                        if latest_gesture != "None Detected":
+                                            # add length of text to start of msg
+                                            msg = f"{len(latest_gesture):<{HEADERSIZE}}" + \
+                                                latest_gesture
+                                            # SEND MESSAGE OVER SOCKET
+                                            clientsocket.send(bytes(msg, "utf-8"))
                                         print(msg)
                                         count += 1
                                 else:
+                                    msg = "none"
+                                    msg = f"{len(msg):<{HEADERSIZE}}" + msg 
+                                    # SEND MESSAGE OVER SOCKET
+                                    clientsocket.send(bytes(msg, "utf-8"))
                                     count = 0
                                     latest_gesture = new_gesture
-                        if cv2.waitKey(5) & 0xFF == 27:
+                        elif cv2.waitKey(5) & 0xFF == 27:
                             break
+                        else:
+                            latest_gesture="None Detected"
+                            msg = "none"
+                            msg = f"{len(msg):<{HEADERSIZE}}" + msg 
+                            # SEND MESSAGE OVER SOCKET
+                            clientsocket.send(bytes(msg, "utf-8"))
+                            continue
+
             except:
                 print("connection lost, looking for new one...")
                 clientsocket.close()
